2428893b37995dbbe254b20127aeb3d981a11c60
Paulo Henrique Pereira Freire <70171639+PauloHenriquePereiraFreire@users.noreply.github.com>
2021-12-30 14:37:22 -0300

    Add files via upload

---
+++ leianumero_while.py
@@ -0,0 +1,7 @@
+#faça um programa que leia um número entre 30 e 70. Repetir a entrada de dados até o usuário digitar um valor válido.
+num=float (input("Digite um número entre 30 e 70:"))
+while num<=30 or num>=70:
+    if num<=30 or num>=70:
+        print ("Valor inválido, digite novamente")
+        num = float(input("Digite um número entre 30 e 70:"))
+print ("Valor válido")
